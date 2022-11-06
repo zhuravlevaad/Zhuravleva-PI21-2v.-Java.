2cda67660c4de8cc8eb5da8603596104e8c4cc3e
zhuravlevaad <114213906+zhuravlevaad@users.noreply.github.com>
2022-11-06 16:23:01 +0300

    Add files via upload

---
+++ Number_1.java
@@ -0,0 +1,14 @@
+// 3. Создайте переменную, увеличьте её на единицу несколькими способами и выведите значение на экран.
+public class Number_1 {
+    public static void main(String[] args) {
+        int a = 4;
+        int a1 = 4;
+        int b = 1;
+        int c = (a + b);
+        int d =++ a;
+        int e = a1 += b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
