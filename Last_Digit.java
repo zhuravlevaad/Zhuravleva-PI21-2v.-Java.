5651b3fbdfdcec99988d5d77ccd1e75b7f6d6294
zhuravlevaad <114213906+zhuravlevaad@users.noreply.github.com>
2022-11-06 17:31:48 +0300

    Add files via upload

---
+++ Last_Digit.java
@@ -0,0 +1,8 @@
+// 6. Дано натуральное число. Выведите его последнюю цифру.
+public class Last_Digit {
+    public static void main(String[] args) {
+        int a = 369;
+        int b = a % 10;
+        System.out.println(b);
+    }
+}
