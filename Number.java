2d3dc05fe160105efc4ef5dab11dc213947a8d10
zhuravlevaad <114213906+zhuravlevaad@users.noreply.github.com>
2022-11-06 15:37:55 +0300

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,7 @@
+// 2. Создайте переменную, присвойте ей целочисленное значение. Выведите значение на экран
+public class Number {
+    public static void main(String[] args) {
+        int x=1;
+        System.out.println(x);
+    }
+}
