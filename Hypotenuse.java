6677195105a13857b9462b7eaf4f8916362ec208
zhuravlevaad <114213906+zhuravlevaad@users.noreply.github.com>
2022-11-06 17:25:38 +0300

    Add files via upload

---
+++ Hypotenuse.java
@@ -0,0 +1,9 @@
+// 5. Дано два числа a и b. Найдите гипотенузу треугольника с заданными катетами.
+public class Hypotenuse {
+    public static void main(String[] args) {
+        double a = 6;
+        double b = 9;
+        double с = a*a + b*b;
+        System.out.println(Math.sqrt(с));
+    }
+}
