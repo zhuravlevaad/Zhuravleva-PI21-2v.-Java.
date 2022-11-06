068f7df30140191ecd51d9a30d88046c0645e288
zhuravlevaad <114213906+zhuravlevaad@users.noreply.github.com>
2022-11-06 17:43:01 +0300

    Add files via upload

---
+++ Ten_2.java
@@ -0,0 +1,8 @@
+// 8. Дано двузначное число. Найдите число десятков в нем.
+public class Ten_2 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = a / 10 % 10;
+        System.out.println(b);
+    }
+}
